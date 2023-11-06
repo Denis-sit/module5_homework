25975aa1fd4b71f2c07d76183abaab37280407c6
Denis Sitnikov <specden990@mail.ru>
2023-11-07 00:15:26 +0500

    Исправленна опечатка

--- task_6.1.js
+++ task_6.1.js
@@ -5,9 +5,9 @@ const str = prompt('Ведите текст', '');
 function weCheckForPolymorphism(item){
     let  str2 = item.split('').reverse().join('');
     if(str.toLocaleLowerCase().replaceAll(' ','') === str2.toLocaleLowerCase().replaceAll(' ','')){
-        console.log(`"${str}" является полиморфом`);
+        console.log(`"${str}" является полиндромом`);
     }else{
-        console.log(`"${str}" не является полиморфом`);
+        console.log(`"${str}" не является полиндромом`);
     }
 };
 
