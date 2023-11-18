0beb6030d724dfe54ca6bd2ef76044a4f4b31759
Denis Sitnikov <specden990@mail.ru>
2023-11-19 01:14:19 +0500

    Создана галлерея картинок

---
+++ gallery.js
@@ -0,0 +1,33 @@
+const btns = document.querySelectorAll('.gallery__button'),
+      gallery = document.querySelector('.gallery__image'),
+      loading = document.querySelector('.gallery__loading');
+
+async function getImage(quantity){
+    loading.style.display = 'flex';
+    try{
+        let res = await fetch(`https://dog.ceo/api/breeds/image/random/${quantity}`);
+        let data = await res.json();
+        data.message.forEach(element => {
+            const image = document.createElement('img');
+            image.src = element;
+            image.classList.add('gallery__image_item');
+            image.onerror = function(){
+                image.remove();
+                console.error('Изображение не загруженно ' + element);
+            }
+            gallery.appendChild(image);
+        });
+    }catch(err){
+        console.log(err.message);
+    }finally{
+        loading.style.display = 'none';
+    }
+};
+
+btns.forEach(item =>{
+    item.addEventListener('click', (e) =>{
+        e.preventDefault;
+        getImage(23);
+    });
+});
+
--- task_3.js
+++
@@ -1,4 +0,0 @@
-// Задание 3
-
-const emptyObj = Object.create(null);
-console.log(Object.getPrototypeOf(emptyObj)); 
